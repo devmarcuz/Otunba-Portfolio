8471e8d54988b9edf27cb6754d6860ef0e80ca42
devmarcuz <otunbamarcusademola@gmail.com>
2024-10-21 15:49:29 +0100

    updated

--- src/components/MainHero.js
+++ src/components/MainHero.js
@@ -5,18 +5,45 @@ import { Link } from "react-router-dom";
 import GrabMagnetic from "./GrabMagnetic";
 import { PiReadCvLogoBold } from "react-icons/pi";
 import Framer from "./CursorMagnetic";
+import { useState } from "react";
 
 const MainHero = ({ stickyElement }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Set initial mobile state based on screen width
+    const updateIsMobile = () => {
+      setIsMobile(window.innerWidth <= 992); // Adjust threshold as per mobile device width (e.g. 768px)
+    };
+
+    // Add event listener for screen resizing
+    window.addEventListener("resize", updateIsMobile);
+    updateIsMobile(); // Initial check when component mounts
+
+    // Clean up event listener on component unmount
+    return () => window.removeEventListener("resize", updateIsMobile);
+  }, []);
+
   return (
     <div className="main-hero">
       <nav>
-        <GrabMagnetic>
+        {!isMobile ? (
+          <GrabMagnetic>
+            <div className="left-nav">
+              <Link to="/" className="h2-container">
+                <h2 className="logo-name hover">
+                  otunba Ademola Otunba-Marcus
+                </h2>
+              </Link>
+            </div>
+          </GrabMagnetic>
+        ) : (
           <div className="left-nav">
             <Link to="/" className="h2-container">
-              <h2 className="logo-name">otunba Ademola Otunba-Marcus</h2>
+              <h2 className="logo-name">otunba</h2>
             </Link>
           </div>
-        </GrabMagnetic>
+        )}
 
         <GrabMagnetic>
           <Link to="" target="_blank" className="icon" ref={stickyElement}>
